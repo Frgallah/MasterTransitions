7550f73e0b2c3e609f6ccbdde9161b136824d218
frgallah <frgallah@outlook.com>
2017-04-19 21:12:59 -0400

    Add all classes to the project

---
+++ Example/MasterTransitions/CustomButtonStyle.swift
@@ -0,0 +1,13 @@
+//
+//  CustomButtonStyle.swift
+//  MasterTransitions
+//
+//  Created by Home on 4/19/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CustomButtonStyle: NSObject {
+
+}
